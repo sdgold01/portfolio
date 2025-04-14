63ccfebdba7b6f2e14bc783cd317c15f5ab35a1c
__sdgold <engrsd22@gmail.com>
2025-04-14 06:36:33 +0100

    changes

--- scripts.js
+++ scripts.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Software Engineer", "Designer", "Freelancer"],
+        strings: ["Test Analyst", "Software Engineer", "Automation Engineer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Software Engineer", "Designer", "Freelancer"],
+        strings: ["Test Analyst", "Software Engineer", "Automation Engineer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
